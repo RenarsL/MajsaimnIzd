cc6a606acf1a90d0b670bfe89792bd18b5eeedf0
Renārs Liepacis <rl2510@gymn.local>
2024-02-21 10:41:56 +0200

    darbs pabeigts

--- MajssaimnIzd/MajssaimnIzd/Form1.Designer.cs
+++ MajssaimnIzd/MajssaimnIzd/Form1.Designer.cs
@@ -28,13 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Ire = new System.Windows.Forms.Label();
+            this.Partika = new System.Windows.Forms.Label();
+            this.Komunalie_m = new System.Windows.Forms.Label();
+            this.Alga = new System.Windows.Forms.Label();
+            this.Papildus_i = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Budzets = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Ire
+            // 
+            this.Ire.AutoSize = true;
+            this.Ire.Location = new System.Drawing.Point(54, 127);
+            this.Ire.Name = "Ire";
+            this.Ire.Size = new System.Drawing.Size(19, 13);
+            this.Ire.TabIndex = 0;
+            this.Ire.Text = "Īre";
+            // 
+            // Partika
+            // 
+            this.Partika.AutoSize = true;
+            this.Partika.Location = new System.Drawing.Point(54, 153);
+            this.Partika.Name = "Partika";
+            this.Partika.Size = new System.Drawing.Size(40, 13);
+            this.Partika.TabIndex = 1;
+            this.Partika.Text = "Pārtika";
+            // 
+            // Komunalie_m
+            // 
+            this.Komunalie_m.AutoSize = true;
+            this.Komunalie_m.Location = new System.Drawing.Point(54, 179);
+            this.Komunalie_m.Name = "Komunalie_m";
+            this.Komunalie_m.Size = new System.Drawing.Size(108, 13);
+            this.Komunalie_m.TabIndex = 2;
+            this.Komunalie_m.Text = "Komunālie maksajumi";
+            // 
+            // Alga
+            // 
+            this.Alga.AutoSize = true;
+            this.Alga.Location = new System.Drawing.Point(54, 49);
+            this.Alga.Name = "Alga";
+            this.Alga.Size = new System.Drawing.Size(28, 13);
+            this.Alga.TabIndex = 3;
+            this.Alga.Text = "Alga";
+            // 
+            // Papildus_i
+            // 
+            this.Papildus_i.AutoSize = true;
+            this.Papildus_i.Location = new System.Drawing.Point(54, 79);
+            this.Papildus_i.Name = "Papildus_i";
+            this.Papildus_i.Size = new System.Drawing.Size(94, 13);
+            this.Papildus_i.TabIndex = 4;
+            this.Papildus_i.Text = "Papildus ieņēmumi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(178, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(83, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(178, 124);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(83, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(178, 72);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(83, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Budzets
+            // 
+            this.Budzets.Location = new System.Drawing.Point(319, 42);
+            this.Budzets.Name = "Budzets";
+            this.Budzets.Size = new System.Drawing.Size(114, 45);
+            this.Budzets.TabIndex = 10;
+            this.Budzets.Text = "Kopējais pieejamais budžets";
+            this.Budzets.UseVisualStyleBackColor = true;
+            this.Budzets.Click += new System.EventHandler(this.Budzets_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(450, 55);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(92, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Budzets);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Papildus_i);
+            this.Controls.Add(this.Alga);
+            this.Controls.Add(this.Komunalie_m);
+            this.Controls.Add(this.Partika);
+            this.Controls.Add(this.Ire);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Ire;
+        private System.Windows.Forms.Label Partika;
+        private System.Windows.Forms.Label Komunalie_m;
+        private System.Windows.Forms.Label Alga;
+        private System.Windows.Forms.Label Papildus_i;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button Budzets;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- MajssaimnIzd/MajssaimnIzd/Form1.cs
+++ MajssaimnIzd/MajssaimnIzd/Form1.cs
@@ -16,5 +16,23 @@ namespace MajssaimnIzd
         {
             InitializeComponent();
         }
+
+        private void Budzets_Click(object sender, EventArgs e)
+        {
+            int Alga = Convert.ToInt32(textBox1.Text);
+            int Papildus_i = Convert.ToInt32(textBox5.Text);
+
+            int ienakumi = Alga + Papildus_i;
+
+            int Ire = Convert.ToInt32(textBox4.Text);
+            int Partika = Convert.ToInt32(textBox3.Text);
+            int Komunalie_m = Convert.ToInt32(textBox2.Text);
+
+            int Izdevumi = Ire + Partika + Komunalie_m;
+
+            double kopejais_b = ienakumi - Izdevumi;
+
+            textBox6.Text = kopejais_b.ToString();
+        }
     }
 }
